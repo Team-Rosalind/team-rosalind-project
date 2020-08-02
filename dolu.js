b8aa5787a74bd14b80f465ebd10bd6f93f5ffbb7
Mosadoluwa-A <magbonyin@gmail.com>
2020-08-02 17:52:21 +0100

    This is Dolu's Javascript Submission for the CSV file

--- dolu.js
+++ dolu.js
@@ -1,11 +1,11 @@
 let Mosa = {
-    name: "Mosadoluwa Agbonyin",
-    email: "magbonyin@gmail.com",
-    language: "Javascript",
-    biostack: "Genomics",
-    slackUsername: "Dolu"
+    name: ": Mosadoluwa Agbonyin",
+    email: ": magbonyin@gmail.com",
+    language: ": Javascript",
+    biostack: ": Genomics",
+    slackUsername: ": Dolu"
 };
 
 for (let [key, value] of Object.entries(Mosa)){
-    console.log(value);
+    console.log(key, value);
 }
