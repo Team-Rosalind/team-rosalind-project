b245b5757554db21d30e19e3a1a8b4d0da1f539e
Suritra Bandyopadhyay <31152879+SuRiTrA@users.noreply.github.com>
2020-08-02 22:38:51 +0530

    Create stage_0_suritra.cpp

---
+++ stage_0_suritra.cpp
@@ -0,0 +1,10 @@
+//This program is for printing the Name, EmailID, Langauge used, Biostack and Slack ID using C++ programming Language using BASH to CSV file
+
+#include <iostream>
+using namespace std;
+//
+int main()
+{
+    cout << "NAME: Suritra Bandyopadhyay\nE-MAIL: suritra.bandyopadhyay@niser.ac.in\nUSED LANGUAGE: C++\nBIOSTACK: Drug Development\nSLACK USERNAME: @Suritra";
+    return 0;
+}
