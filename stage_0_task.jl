ffd7f8fae13a98a67f20b128d1c99039da55713f
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2020-08-02 15:10:39 +0300

    Rename stage_0_task.jl to stage_0_pavlo.jl

--- stage_0_task.jl
+++
@@ -1,4 +0,0 @@
-# This script prints out HackBio participant data as a part of stage 0 task.
-# AUTHOR: Pavlo Hrab
-
-print("NAME: Pavlo Hrab\nE-MAIL: pavlo.hrab@lnu.edu.ua\nUSED LANGUAGE: JULIA\nBIOSTACK: Genomics\nSLACK USERNAME: @pavlo\n")
