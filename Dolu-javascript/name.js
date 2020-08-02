f3eb73f0c93eb3b1c404df3e38514acc34a82bb3
Mosadoluwa-A <magbonyin@gmail.com>
2020-08-02 14:56:39 +0100

    This is Dolu's Javascript submission

---
+++ Dolu-javascript/name.js
@@ -0,0 +1,11 @@
+let Mosa = {
+    name: "Mosadoluwa Agbonyin",
+    email: "magbonyin@gmail.com",
+    language: "Javascript",
+    biostack: "Genomics",
+    slackUsername: "Dolu"
+};
+
+for (let [key, value] of Object.entries(Mosa)){
+    console.log(value);
+}
