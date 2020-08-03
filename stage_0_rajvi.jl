4bf120672b190029646938eb47994d06ba13ec14
rajvisrivastav1905 <53811342+rajvisrivastav1905@users.noreply.github.com>
2020-08-03 12:27:32 +0530

    First commit
    information using julia

---
+++ stage_0_rajvi.jl
@@ -0,0 +1,4 @@
+info = Dict("NAME: " => "Rajvi Srivastav", "EMAIL: " => "rajvisrivastav18@gmail.com", "LANGUAGE: " => "JULIA", "BIOSTACK: " => "Genomics", "SLACK USERNAME: " => "@Rajvi")
+ for (i, j) in info
+       println(i, j)
+       end
