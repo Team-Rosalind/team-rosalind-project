8a79d5178517b4a4c3eed6cadc30f1a9dfbe42bc
RajviS1904 <48009156+RajviS1904@users.noreply.github.com>
2020-08-03 14:49:36 +0530

    Update stage_0_rajvi.jl

--- stage_0_rajvi.jl
+++ stage_0_rajvi.jl
@@ -1,4 +1,4 @@
-info = Dict("NAME: " => "Rajvi Srivastav", "EMAIL: " => "rajvisrivastav18@gmail.com", "LANGUAGE: " => "JULIA", "BIOSTACK: " => "Genomics", "SLACK USERNAME: " => "@Rajvi")
+info = Dict("NAME: " => "Rajvi Srivastav", "EMAIL: " => "rajvisrivastav18@gmail.com", "USED LANGUAGE " => "JULIA", "BIOSTACK: " => "Genomics", "SLACK USERNAME: " => "@Rajvi")
  for (i, j) in info
        println(i, j)
        end
