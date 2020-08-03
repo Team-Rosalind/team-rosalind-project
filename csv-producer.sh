5efba1279a7a5669e40821d4af21585c19a95a21
H <51130661+Harinath1232001@users.noreply.github.com>
2020-08-03 13:48:05 +0530

    Update csv-producer.sh

--- csv-producer.sh
+++ csv-producer.sh
@@ -15,6 +15,7 @@ Rscript stage_0_shirine.R |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} 
 ./Suritra |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Suritra.csv;
 python Stage_0_Leye.py|  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Leye.csv;
 Rscript Stage_0_Ife.R |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Ife.csv;
+Cprogram stage_0_harinath.C | awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > harinath.csv;
 
 
 #Adds newlines at the end of csv files
@@ -26,6 +27,7 @@ echo "" >>hildred.csv;
 echo "" >>Leye.csv;
 echo "" >>pavlo.csv;
 echo "" >>Anu.csv;
+echo "" >>harinath.csv;
 #Merge csvs
 cat *.csv > Member_info_team-rosalind.txt;
 #rm -f *.csv && mv Member_info_team-rosalind.txt Member_info_team-rosalind.csv
