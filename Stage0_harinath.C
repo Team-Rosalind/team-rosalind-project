5215df12a4301b755310d587573d7c9e74ec5313
H <51130661+Harinath1232001@users.noreply.github.com>
2020-08-03 12:10:59 +0530

    Update Stage0_harinath.C

--- Stage0_harinath.C
+++ Stage0_harinath.C
@@ -1,4 +1,4 @@
 #include<stdio.h>
 int main{
-    printf("NAME : HARINATH\nEmail : harinath1232001@gmail.com\nLANGUAGE_USED : C_PROGRAMMING\nBIOSTACK : GENOMICS\nSLACK_Username : Harinath");
+    printf("NAME : HARINATH\nEmail : harinath1232001@gmail.com\nLANGUAGE_USED : C_PROGRAMMING\nBIOSTACK : GENOMICS\nSLACK_Username : @Harinath");
 }
