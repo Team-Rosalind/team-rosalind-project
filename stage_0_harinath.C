27e97acae17144483e064ac676046e26b40b0451
H <51130661+Harinath1232001@users.noreply.github.com>
2020-08-03 13:50:58 +0530

    Update stage_0_harinath.C

--- stage_0_harinath.C
+++ stage_0_harinath.C
@@ -1,4 +1,5 @@
 #include<stdio.h>
 int main{
     printf("NAME : HARINATH\nEmail : harinath1232001@gmail.com \nLANGUAGE_USED : C_PROGRAMMING\n BIOSTACK : GENOMICS \nSLACK_Username : @Harinath");
+    return 0;
 }
