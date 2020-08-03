49a0281238b5de7ffe7aac35ae21ca5f6f9ecd9d
AnnapoornaChamundi <69087190+AnnapoornaChamundi@users.noreply.github.com>
2020-08-03 12:16:30 +0530

    Update stage_0_Anu.C

--- stage_0_Anu.C
+++ stage_0_Anu.C
@@ -2,6 +2,6 @@
 
 void main()
 {
-    printf("Name: Annapoorna C\n Email:annapoornachamundi23@gmail.com\n Language used: C Programming\n Biostack: Drug discovery and development");
+    printf("Name: Annapoorna C\n Email:annapoornachamundi23@gmail.com\n Language used: C Programming\n Biostack: Drug discovery and development\n Slack_username: @Anu");
 
 }
