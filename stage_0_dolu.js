72398d9f44754a306dd60cda7af9aa34b43379ff
Mosadoluwa-A <magbonyin@gmail.com>
2020-08-03 09:04:03 +0100

    Updated code to conform with pipeline parameters

--- stage_0_dolu.js
+++ stage_0_dolu.js
@@ -1,9 +1,9 @@
 let Mosa = {
-    name: ": Mosadoluwa Agbonyin",
-    email: ": magbonyin@gmail.com",
-    language: ": Javascript",
-    biostack: ": Genomics",
-    slackUsername: ": Dolu"
+    NAME: ": Mosadoluwa Agbonyin",
+    EMAIL: ": magbonyin@gmail.com",
+    USED_LANGUAGE: ": Javascript",
+    BIOSTACK: ": Genomics",
+    SLACK_USERNAME: ": @Dolu"
 };
 
 for (let [key, value] of Object.entries(Mosa)){
