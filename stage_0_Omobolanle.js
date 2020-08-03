864a942614bd6c6c2fbb000255aca05896662e70
Omobolanle1923 <68826314+Omobolanle1923@users.noreply.github.com>
2020-08-03 20:40:30 +0100

    stage_0_Omobolanle.js

---
+++ stage_0_Omobolanle.js
@@ -0,0 +1,14 @@
+let script = {
+    Name: "Abiodun Omobolanle Abimbola",
+    email_Address: "abimbola1923@gmail.com",
+    language: "javascript",
+    biostack: "Medicinal Chemistry",
+    slackUsername: "@omobolanle19"
+
+}
+
+console.log(script.Name);
+console.log(script.email_Address);
+console.log(script.language);
+console.log(script.biostack);
+console.log(script.slackUsername);
