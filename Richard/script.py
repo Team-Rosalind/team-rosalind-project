ea9c0e9d85d2ad21b7c650a6a9e1e4aae429438e
Richard-code-gig <69102122+Richard-code-gig@users.noreply.github.com>
2020-08-04 13:44:34 +0100

    Delete script.py

--- Richard/script.py
+++
@@ -1,8 +0,0 @@
-My_Name = "Sola Richard Olorunfemi"
-print (My_Name)
-Email = "Richardolorunfemi17@gmail.com"
-print (Email)
-Biostack = "Stage 0 | Genomics"
-print (Biostack)
-Username = "@Richard"
-print (Username)
