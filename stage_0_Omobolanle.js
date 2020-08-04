df39aa081cda45a7cac5a61c247d04191f3b7259
Omobolanle1923 <68826314+Omobolanle1923@users.noreply.github.com>
2020-08-04 18:03:13 +0100

    Add files via upload
    stage_0_Omobolanle.js

---
+++ stage_0_Omobolanle.js
@@ -0,0 +1,15 @@
+let script = {
+    Name: "NAME: ABIODUN OMOBOLANLE ABIMBOLA",
+    email_Address: "emailaddress: abimbola1923@gmail.com",
+    language: "language: JAVASCRIPT",
+    biostack: "biostack: Medicinal Chemistry",
+    slackUsername: "slackUsername: @omobolanle19"
+
+}
+
+
+console.log(script.Name);
+console.log(script.email_Address);
+console.log(script.language);
+console.log(script.biostack);
+console.log(script.slackUsername);
