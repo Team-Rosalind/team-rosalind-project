7526e05fd617d2c886fd6db125096982ac0ed27a
RajviS1904 <48009156+RajviS1904@users.noreply.github.com>
2020-08-04 15:46:02 +0530

    Update stage_0_rajvi.jl
    Updated dict to array for ordered output

--- stage_0_rajvi.jl
+++ stage_0_rajvi.jl
@@ -1,4 +1,4 @@
-info = Dict("NAME: " => "Rajvi Srivastav", "EMAIL: " => "rajvisrivastav18@gmail.com", "USED LANGUAGE " => "JULIA", "BIOSTACK: " => "Genomics", "SLACK USERNAME: " => "@Rajvi")
- for (i, j) in info
+list = ["NAME: Rajavee Srivastava","EMAIL: rajvisrivastav18@gmail.com","USED LANGUAGE: JULIA", "BIOSTACK: Genomics","SLACK USERNAME: @Rajvi"]
+ for (i, j) in list
        println(i, j)
        end
