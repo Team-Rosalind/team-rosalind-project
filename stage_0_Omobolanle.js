9a3ce55b6e27e383897279e7dc532e296cb0c8c4
Omobolanle1923 <68826314+Omobolanle1923@users.noreply.github.com>
2020-08-04 23:15:06 +0100

    Add files via upload

---
+++ stage_0_Omobolanle.js
@@ -0,0 +1,13 @@
+let script = {
+    NAME: " Abiodun Omobolanle Abimbola",
+    EMAIL: " abimbola1923@gmail.com",
+    USED_LANGUAGE: " JAVASCRIPT",
+    BIOSTACK: "Medicinal Chemistry",
+    SLACK_USERNAME: "@omobolanle19"
+
+}
+
+
+for (let [key, value] of Object.enteries(script)) {
+    console.log(key, value);
+}
