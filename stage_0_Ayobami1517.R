f9cfb37028fb230113aa7431e05003c11f46f155
ayobami1517 <69127031+ayobami1517@users.noreply.github.com>
2020-08-05 00:46:01 +0100

    Stage_0_script

---
+++ stage_0_Ayobami1517.R
@@ -0,0 +1,8 @@
+name <- "Ayobami Elizabeth Adeniyi"
+email <- "ayobamiadeniyi1517@gmail.com"
+biostack <- "Drug Discovery"
+username <- "@Ayobami1517"
+language <- "R"
+
+cat(paste("name:", name, "email:", email, "biostack:", biostack, "username:", 
+          username, "language:", language, Sep="ln"))
