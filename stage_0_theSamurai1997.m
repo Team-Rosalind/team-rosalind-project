7f6e46c3a793c30b462416773750a359259b5c02
Karan Kumar <52522190+theSamurai1997@users.noreply.github.com>
2020-08-04 16:28:32 +0530

    Update stage_0_theSamurai1997.m
    Updated Name

--- stage_0_theSamurai1997.m
+++ stage_0_theSamurai1997.m
@@ -8,8 +8,8 @@ biostack = "Genomics";
 username = "@theSamurai1997";
 
 % Setting up and formatting the printing messages
-my_name = sprintf("Name : %s", name);
-my_email = sprintf("E-mail ID : %s", email);
+my_name = sprintf("NAME : %s", name);
+my_email = sprintf("E-MAIL ID : %s", email);
 my_language = sprintf("Programming Language : %s", language);
 my_biostack = sprintf("BIOSTACK : %s", biostack);
 my_username = sprintf("Slack UserID : %s", username);
