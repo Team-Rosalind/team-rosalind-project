55efa6d96a993ab3c4a783a24a82580db3800e99
RajviS1904 <48009156+RajviS1904@users.noreply.github.com>
2020-08-04 15:47:04 +0530

    Update stage_0_rajvi.jl

--- stage_0_rajvi.jl
+++ stage_0_rajvi.jl
@@ -1,4 +1,4 @@
 list = ["NAME: Rajavee Srivastava","EMAIL: rajvisrivastav18@gmail.com","USED LANGUAGE: JULIA", "BIOSTACK: Genomics","SLACK USERNAME: @Rajvi"]
- for (i, j) in list
-       println(i, j)
+ for i in list
+       println(i)
        end
