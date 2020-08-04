71235fab2e9a0c8db8773bd5bfaee54773495ca3
Success Ologunsua <successologunsua@gmail.com>
2020-08-04 01:59:12 +0100

    Add files via upload
    Added A .py extension of my script as opposed the earlier submission which has an .ipynb extension.

---
+++ Stage 0_TheSussex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("NAME: Success Ologunsua, \nEMAIL: successologunsua@gmail.com, \nLANGUAGE USED: Python, \nBIOSTACK: Data Analytics, \nSLACK USERNAME: @TheSussex")
+
+
+# In[ ]:
+
+
+
+
