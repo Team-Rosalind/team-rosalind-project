56205cca71c85138411a4094fc381d1a16bdbc60
sheen1595 <69086625+sheen1595@users.noreply.github.com>
2020-08-04 01:43:00 -0500

    Create stage_0_Nicole.R

---
+++ stage_0_Nicole.R
@@ -0,0 +1,11 @@
+
+name <- 'Nicole Sheen Sanchez'
+email <- "alexandra.sheen@unmsm.edu.pe"
+language <- 'R'
+biostack <- "Genomics"
+slackuser <- "@Nicole"
+data <- data.frame(Information=c(name, email, language, biostack, slackuser))
+rownames(data) <- c('NAME', 'E-MAIL', 'USED LANGUAGE', 'BIOSTACK', 'SLACK USERNAME')
+print(data)
+
+
