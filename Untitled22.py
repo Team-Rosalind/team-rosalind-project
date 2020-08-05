8d7e0a26f8c25c264a22d721f5e5a88c7ff19305
Richard-code-gig <69102122+Richard-code-gig@users.noreply.github.com>
2020-08-05 09:42:28 +0100

    Delete Untitled22.py

--- Untitled22.py
+++
@@ -1,5 +0,0 @@
-print('NAME: Sola Richard Olorunfemi ')
-print('E-MAIL: Richardolorunfemi17@gmail.com')
-print('USED LANGUAGE: PYTHON')
-print('BIOSTACK: Genomics')
-print('SLACK USERNAME: @Richard')
