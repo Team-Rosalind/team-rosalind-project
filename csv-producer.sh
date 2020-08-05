9a8e9665f13a4eb2721056047d1783b192392953
Pavlo <paul.hrab05@gmail.com>
2020-08-05 11:11:28 +0300

    change csv-producer to parallel execution

--- csv-producer.sh
+++ csv-producer.sh
@@ -34,41 +34,37 @@ git clone https://github.com/Team-Rosalind/team-rosalind-project.git && cd team-
 
 #run your scripts. Output is 1 line csv file
 echo "Running compilers for C and C++ files..."
-g++ -o Suritra stage_0_Suritra.cpp;
-gcc -o stage_0_Anu stage_0_Anu.C ;
-gcc -o stage_0_harinath stage_0_harinath.C ;
+ls *.cpp | parallel g++ -o {.}.cpp-program {};
+ls *.C | parallel gcc -o {.}.c-program {};
 echo "Running main scripts..."
-julia stage_0_pavlo.jl |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2  > pavlo.csv;
-./stage_0_Anu |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Anu.csv;
-node stage_0_dolu.js  |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > dolu.csv;
-python stage_0_hildred.py |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > hildred.csv;
-Rscript stage_0_shirine.R |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > shirine.csv;
-./Suritra |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Suritra.csv;
-python Stage_0_Leye.py|  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Leye.csv;
-Rscript Stage_0_Ife.R |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Ife.csv;
-./stage_0_harinath | awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > harinath.csv;
-python stage_0_rupa.py|  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > rupa.csv;
-octave stage_0_Vika.m | awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Vika.csv;
-python Stage_0_Busayo.py|  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Busayo.csv;
-julia stage_0_rajvi.jl |  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2  > rajvi.csv;
-python stage_0_siddhant.py|  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > siddhant.csv;
-octave stage_0_theSamurai1997.m | awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > theSamurai1997.csv;
-python Stage_0_Richard.py|  awk -F ": " '/^NAME/ {print ""} {printf "%s,", $2} ' | tail -n +2 > Richard.csv;
-#Adds newlines at the end of csv files
+ls *.jl | parallel "julia {} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
+ls *.py | parallel "python {} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
+ls *.m | parallel "octave {} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
+ls *.js | parallel "node {} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
+ls *.R | parallel "Rscript {} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
+ls *.cpp-program | parallel "./{} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
+ls *.c-program | parallel "./{} |awk -F ': ' ' {print \$2} ' |paste -sd ','  > {.}.lines";
 echo "Running postprocessing of made csv files..."
-for i in *.csv; do echo "" >> $i; done 
+#for i in *.csv; do echo "" >> $i; done ;
+# Removing empty columns from files
+for i in *.lines; do cat $i | sed 's/,\s,/,/g' |sed 's/^,//g' | sed 's/,\s*/,/g' | sed 's/^\s*//g'  > $i.csv; done;
+#rm -f *.csv ;
 #Merge csvs
-echo "Merging csv files..."
-cat *.csv > Member_info_team-rosalind.txt;
+echo "Merging csv files...";
+#cat *.tsvs > Member_info_team-rosalind.txt;
+cat `ls *.csv | paste	-sd '\t'` > Member_info_team-rosalind.txt
 #Remove temporary files
 echo "Removing temporary files..."
-rm -f *.csv 
-rm -f Suritra
-rm -f stage_0_Anu 
-rm -f stage_0_harinath
+rm -f *.lines ;
+rm -f *.csv ;
+rm -f *.c-program;
+rm -f *.cpp-program ;
 #Rename txt file to csv one
 echo "Renaming final file..."
-mv Member_info_team-rosalind.txt Member_info_team-rosalind.csv
+# Remove blasnk rows from final file
+cat Member_info_team-rosalind.txt | awk -F, 'length>NF+1' > Member_info_team-rosalind.csv;
+# Remove temporary file
+rm -f Member_info_team-rosalind.txt;
 echo "Done!"
 echo ""
 echo -e "\033[1mPlease consider using Member_info_team-rosalind.csv file in repo directory"
