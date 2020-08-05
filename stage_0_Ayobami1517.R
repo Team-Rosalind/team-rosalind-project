2d69b3950216b8c7b1571fa3ce2c0e568f5934af
ayobami1517 <69127031+ayobami1517@users.noreply.github.com>
2020-08-05 11:27:54 +0100

    Delete stage_0_Ayobami1517.R

--- stage_0_Ayobami1517.R
+++
@@ -1,8 +0,0 @@
-name <- "Ayobami Elizabeth Adeniyi"
-email <- "ayobamiadeniyi1517@gmail.com"
-biostack <- "Drug Discovery"
-username <- "@Ayobami1517"
-language <- "R"
-
-cat(paste("name:", name, "email:", email, "biostack:", biostack, "username:", 
-          username, "language:", language, Sep="ln"))
