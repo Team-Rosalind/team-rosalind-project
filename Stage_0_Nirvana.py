1fb776705bc46dade02ab5ab346ef7bbea4ea8c9
Nirvana-Munir <69080911+Nirvana-Munir@users.noreply.github.com>
2020-08-05 10:30:08 +0200

    Update Stage_0_Nirvana.py

--- Stage_0_Nirvana.py
+++ Stage_0_Nirvana.py
@@ -1,2 +1,2 @@
 #member data
-print("NAME: Nirvana Munir \E-MAIAL: nirvana.monir66@gmail.com \USED-LANGUAGE: PYTHON \BIOSTACK:Medicinal Chemistry \SLACK USERNAME: @Nirvana") 
+print("NAME: Nirvana Munir \nE-MAIAL: nirvana.monir66@gmail.com \nUSED-LANGUAGE: PYTHON \nBIOSTACK:Medicinal Chemistry \nSLACK USERNAME: @Nirvana") 
