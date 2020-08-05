ca384ec57b11b3a2f6f0570e8a0061fca4fa9f8d
Pavlo <paul.hrab05@gmail.com>
2020-08-05 09:24:36 +0300

    Improve tests script + nicole.R

--- tests.sh
+++ tests.sh
@@ -107,8 +107,56 @@ then
     echo ""
     exit
 fi;
+if ! command -v sed &> /dev/null
+then
+    echo "sed could not be found"
+    echo "Please run for Ubuntu-based systems:"
+    echo ""
+    echo "sudo apt-get install sed" 
+    echo ""
+    echo "or for Arch-based systems:"
+    echo ""
+    echo "sudo pacman -S sed"
+    echo ""
+    exit
+fi;
+if ! command -v parallel &> /dev/null
+then
+    echo "parallel could not be found"
+    echo "Please run for Ubuntu-based systems:"
+    echo ""
+    echo "sudo apt-get install parallel" 
+    echo ""
+    echo "or for Arch-based systems:"
+    echo ""
+    echo "sudo pacman -S parallel"
+    echo ""
+    exit
+fi;
 echo "Done!"
 echo "All requirements satisfied!"
+echo "Running test scripts..."
+cd tests;
+ls *.py | parallel  "python {} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+ls *.m | parallel  "octave {} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+for i in *.C; do gcc -o $i.c-program $i; done;
+ls *.c-program | parallel  "./{} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+for i in *.cpp; do g++ -o $i.cpp-program $i; done;
+ls *.cpp-program | parallel  "./{} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+ls *.R | parallel  "Rscript {} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+ls *.jl | parallel  "julia {} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+ls *.js | parallel  "node {} |awk -F ': ' ' {print \$2} ' |paste -sd ',' > {.}.csv";
+rm -f *.cpp-program;
+rm -f *.c-program;
+cat *.csv > Tests.txt;
+rm -f *.csv && mv Tests.txt test.csv;
+cd ..;
+
+echo ""
+echo "Please check the output from tests in tests/test.csv"
+echo "All seven languages must be in the csv file (C, C++, MATLAB, PYTHON, R, JULIA, JAVASCRIPT) and all cells must be filled!"
+echo "Best Regards,"
+
 cat << "EOF"
 
  _____                      ____                 _ _           _ 
