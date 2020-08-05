ae1bc04bd3eb4e09b1187eb6057d4bc635029f17
Suritra Bandyopadhyay <31152879+SuRiTrA@users.noreply.github.com>
2020-08-06 01:03:43 +0530

    A small error fix

--- Stage_0_Ayobami1517.R
+++ Stage_0_Ayobami1517.R
@@ -7,5 +7,6 @@ language <- "R"
 data <- data.frame(Information=c(name, email, biostack, language, username))
 
 rownames(data) <- c('NAME: ', 'E-MAIL: ', 'BIOSTACK: ', 'LANGUAGE: ', 'USERNAME: ')
+
 colnames(data) <- NULL
 print(data)
