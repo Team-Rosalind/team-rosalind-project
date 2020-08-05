306a803be9f6e2e5073ab29df17ac5201c5959ea
AlaaSamy92 <69093973+AlaaSamy92@users.noreply.github.com>
2020-08-05 17:23:53 +0200

    Update stage_0_Alaa.py

--- stage_0_Alaa.py
+++ stage_0_Alaa.py
@@ -2,9 +2,9 @@
 import pandas as pd
 data= pd.read_excel("stage_0_Alaa.xlsx")
 for my_data in data.iloc:
-    print('NAME:{}'.format(my_data[0]))
-    print('E_MAIL:{}'.format(my_data[1]))
-    print('USED LANGUAGE:{}'.format(my_data[2]))
-    print('BIOSTACK:{}'.format(my_data[3]))
-    print('SLACK USERNAME:@{}'.format(my_data[4]))
+    print('NAME: {}'.format(my_data[0]))
+    print('E_MAIL: {}'.format(my_data[1]))
+    print('USED LANGUAGE: {}'.format(my_data[2]))
+    print('BIOSTACK: {}'.format(my_data[3]))
+    print('SLACK USERNAME: @{}'.format(my_data[4]))
 
