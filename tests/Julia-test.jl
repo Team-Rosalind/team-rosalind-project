39c32a49c7b8b12e6e017d2c4a0f549369d4b7e6
Pavlo <paul.hrab05@gmail.com>
2020-08-05 11:10:50 +0300

    Added tests

---
+++ tests/Julia-test.jl
@@ -0,0 +1,3 @@
+# This script prints out HackBio participant data as a part of stage 0 task.
+
+print("name: Jeff Bezos\nE-MAIL: jeff@lnu.edu.ua\nUSED LANGUAGE: JULIA\nBIOSTACK: Chemoinformatics\nSLACK USERNAME: @jeff\n")
